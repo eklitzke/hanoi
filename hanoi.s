a6652eff8b1ea2a9ae535292b5fb1bb85550f4b9
Evan Klitzke <evan@eklitzke.org>
2016-10-15 18:54:59 -0700

    solve one step

--- hanoi.s
+++ hanoi.s
@@ -8,11 +8,39 @@ newline:   .ascii "\n"
 
   .text
 
-  ##  zero the memory in a tower
+###  zero the memory in a tower
 init_tower:
   xor %rax, %rax                # zero %rax
-  movl $11, %ecx
-  rep stosw
+  movl $64, %ecx
+  rep stosb
+  ret
+
+###  peek the top of the tower
+peek_tower:
+  movl (%rdi),%ecx
+  cmpl $0, %ecx
+  jz peek_empty
+  dec %ecx
+  movq 4(%rdi, %rcx, 4), %rax
+  ret
+peek_empty:
+  movl $100000, %eax
+  ret
+
+pop_tower:
+  movl (%rdi),%ebx
+  dec %ebx
+ 	movl 4(%rdi, %rbx, 4), %eax   # copy the value to %rax
+  movl $0, 4(%rdi, %rbx, 4)     # zero the value
+  mov %ebx, (%rdi)              # decrease count
+  ret
+
+### the tower in %rdi, the val in %rsi
+push_tower:
+  movl (%rdi),%ecx
+  mov %rsi, 4(%rdi, %rcx, 4)
+  inc %ecx
+  mov %ecx, (%rdi)
   ret
 
 print_one:
@@ -67,7 +95,31 @@ print_all:
   call print_newline
   ret
 
-  ## Args: number of rings
+### rdi and rsi should be towers to move between
+move_tower:
+  mov %rdi, %r9
+  call peek_tower
+  mov %rax, %r10
+  mov %rsi, %rdi
+  call peek_tower
+  mov %r9, %rdi
+  cmp %rax, %r10
+  jl less_branch
+greater_branch:
+  int3
+less_branch:
+  // source is rdi, dest is rsi
+  call pop_tower
+  push %rdi
+  push %rsi
+  mov %rsi, %rdi
+  mov %rax, %rsi
+  call push_tower
+  pop %rsi
+  pop %rdi
+  ret
+
+### %rdi is the number of rings
 solve:
   push %rdi
   mov %rsp, %rbp
@@ -87,20 +139,36 @@ solve:
   mov %rsp, %rdi
   call init_tower
 
-fill_tower:
   // initialize S tower
   lea -64(%rbp), %rax
   mov (%rbp),%rcx
-  mov %rcx, (%rax)                # set the size of the tower
+  mov %rcx, (%rax)              # set the size of the tower
   add $4, %rax
+
 init_s:
   mov %rcx, (%rax)
   add $4, %rax
   loop init_s
 
-  // print the towers
+  call print_all                # print them all
+
+  // calculate the loop count, and push it onto the stack
+  // this is known to be: (1<<N) - 1
+  mov (%rbp), %cl
+  movl $1, %eax
+  shl %cl, %rax
+  dec %eax
+  push %rax
+
+solve_loop:
+  lea -64(%rbp), %rdi
+  lea -128(%rbp), %rsi
+  call move_tower
+
   call print_all
-  lea 8(%rbp), %rsp
+
+foo:
+  lea 8(%rbp), %rsp             # fix the stack
   ret
 
 _start:
